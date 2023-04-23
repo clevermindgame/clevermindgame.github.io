c5c5f2317cda5f1dff3ce5dc9eea4ff1363bd034
clevermindgame <130675847+clevermindgame@users.noreply.github.com>
2023-04-23 06:39:33 +0200

    Updated from Publii

--- media/files/gscript.js
+++ media/files/gscript.js
@@ -1,79 +1,86 @@
 // dimensioni dello schermo
 hWindow = window.innerHeight;
 wWindow = window.innerWidth;
-rWindow = hWindow/wWindow;
+rWindow = hWindow / wWindow;
 //
 // variabili per le funzioni di gioco
 cellaHTML = new Array(64);
+cellaVSBL = new Array(64);
 //
 // Adatta la scacchiera alla dimensione dello schermo
 // fino a un rapporto H/W di 1.4, la larghezza della scacchiera è dell'80%
 // oltre un rapporto H/W di 1.8, la larghezza della scacchiera è del 97%
 // nell'intervallo tra 1.4 e 1.8, la larghezza varia in proporzione
-scacchieraW = Math.round(80 + (rWindow - 1.4)*17/0.4);
-if (rWindow < 1.4) {scacchieraW = 75};
-if (rWindow > 1.8) {scacchieraW = 97};
+scacchieraW = Math.round(80 + ((rWindow - 1.4) * 17) / 0.4);
+if (rWindow < 1.4) {
+    scacchieraW = 80;
+}
+if (rWindow > 1.8) {
+    scacchieraW = 97;
+}
 scacchiera.style.width = scacchieraW + "%";
 //
 // Crea la scacchiera
-n = 8
+n = 8;
 creaScacchiera(n);
 //
 // Dimensiona l'area dello storico
 const storicoDiv = document.getElementById("storico");
 const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 const divTop = storicoDiv.offsetTop;
-const remainingHeight = hWindow - (divTop - scrollTop)-10;
+const remainingHeight = hWindow - (divTop - scrollTop) - 10;
 storicoDiv.style.height = `${remainingHeight}px`;
 //
-storicoDiv.insertAdjacentHTML("beforeend","<br />--- fine ---");
 stampaUA();
-logga();
-//
-//  =======>
-aggiungiEventi();
+aggiungiEventiDialoghi();
+aggiungiEventiScacchiera();
 //
 // Funzioni
 //
 // crea scacchiera
 function creaScacchiera(n) {
-    mostra = true
-// Cancella la scacchiera attuale
+    mostra = true;
+    document.getElementById("mostra").textContent = "Nascondi";
+    // Cancella la scacchiera attuale
     scacchiera = document.getElementById("scacchiera");
     var primariga = scacchiera.rows[0];
-    for (i = scacchiera.rows.length - 1; i>=0; i--) {
+    for (i = scacchiera.rows.length - 1; i >= 0; i--) {
         scacchiera.deleteRow(i);
     }
-//   for (i = primariga.cells.length - 1; i>0; i--) {
-//        primariga.deleteCell(i);
-//    }
+    //   for (i = primariga.cells.length - 1; i>0; i--) {
+    //        primariga.deleteCell(i);
+    //    }
 
-// Crea le nuove righe
-    lcella = Math.trunc((wWindow * scacchieraW/100-n-1)/n)
-    charSize = Math.trunc(lcella/3)
+    // Crea le nuove righe
+    lcella = Math.trunc(((wWindow * scacchieraW) / 100 - n - 1) / n);
+    charSize = Math.trunc(lcella / 3);
     for (i = 0; i < n; i++) {
         let riga = scacchiera.insertRow(i);
         for (j = 0; j < n; j++) {
-          let cella = riga.insertCell(j);
-          cella.id = n * i + j;
-          cella.classList.add("cinterno");
-	  cella.style.fontSize = charSize + "px";
-          cellaHTML[n*i+j] = ""
-	  if ((j == 0) || (j == n-1)) {
-		cella.classList.replace("cinterno","cbordo");
-		cellaHTML[n*i+j] = String.fromCharCode(j+65,i+49);
-                cella.innerHTML = cellaHTML[n*i+j]
-	  }
-	  if ((i == 0) || (i == n-1)) {
-		cella.classList.replace("cinterno","cbordo");
-		cellaHTML[n*i+j] = String.fromCharCode(j+65,i+49);
-                cella.innerHTML = cellaHTML[n*i+j]
-	  }
-	  if (((i == 0) || (i == n-1)) && ((j == 0) || (j == n-1))) {
-		cella.classList.replace("cbordo","cangolo");
-                cellaHTML[n*i+j] = cellaHTML[n*i+j]
-		cella.innerHTML = String.fromCharCode(j+65,i+49);
-	  }
+            let cella = riga.insertCell(j);
+            cella.id = n * i + j;
+            cella.classList.add("cinterno");
+            cella.style.fontSize = charSize + "px";
+            cellaHTML[n * i + j] = "";
+            cellaVSBL[n * i + j] = false;
+            if (j == 0 || j == n - 1) {
+                cella.classList.replace("cinterno", "cbordo");
+                cellaHTML[n * i + j] = String.fromCharCode(j + 65, i + 49);
+                cella.innerHTML = cellaHTML[n * i + j];
+                cellaVSBL[n * i + j] = true;
+            }
+            if (i == 0 || i == n - 1) {
+                cella.classList.replace("cinterno", "cbordo");
+                cellaHTML[n * i + j] = String.fromCharCode(j + 65, i + 49);
+                cella.innerHTML = cellaHTML[n * i + j];
+                cellaVSBL[n * i + j] = true;
+            }
+            if ((i == 0 || i == n - 1) && (j == 0 || j == n - 1)) {
+                cella.classList.replace("cbordo", "cangolo");
+                cellaHTML[n * i + j] = String.fromCharCode(j + 65, i + 49);
+                cella.innerHTML = cellaHTML[n * i + j];
+                cellaVSBL[n * i + j] = true;
+            }
         }
     }
 
@@ -83,133 +90,172 @@ function creaScacchiera(n) {
         celle[i].style.width = lcella + "px";
     }
 }
-function aggiungiEventi() {
-celle_s = document.querySelectorAll(".cinterno");
-dialogS = document.getElementById('imageDialog');
-celle_s.forEach(cell => {
-	cell.addEventListener('click', () => {
-	cella = cell
-	cellaID = cell.id;
-	if (mostra) {
-	   dialogS.showModal();
-           };
-	});
-});
-    image0.addEventListener('click', () => {
-      cella.innerHTML = '';
-      cellaHTML[cellaID] = "";
-      dialogS.close();
+function rimuoviEventiScacchiera() {
+    celle_s = document.querySelectorAll(".cinterno");
+    celle_s.forEach((cell) => {
+    	const handleS = () => {
+            cella = cell;
+            cellaID = cell.id;
+            dialogS.showModal();
+        };
+        cell.removeEventListener("click", handleS);
+  });
+    celle_b = document.querySelectorAll(".cbordo");
+    celle_b.forEach((cell) => {
+    	const handleB = () => {
+            cella = cell;
+            cellaID = cell.id;
+            dialogB.showModal();
+        };
+        cell.removeEventListener("click", handleB);
+  });
+}
+function aggiungiEventiScacchiera() {
+    celle_b = document.querySelectorAll(".cbordo");
+    dialogB = document.getElementById("bordoDialog");
+    celle_b.forEach((cell) => {
+    	const handleB = () => {
+            cella = cell;
+            cellaID = cell.id;
+            dialogB.showModal();
+       };
+       cell.addEventListener("click", handleB);
     });
-    image1.addEventListener('click', () => {
-console.log(cella);
-      cellaHTML[cellaID] = '<img src="cb.png" style="width="100%" height="100%">';
-      cella.innerHTML = cellaHTML[cellaID];
-      dialogS.close();
+    celle_s = document.querySelectorAll(".cinterno");
+    dialogS = document.getElementById("imageDialog");
+    celle_s.forEach((cell) => {
+    	const handleS = () => {
+            cella = cell;
+            cellaID = cell.id;
+            dialogS.showModal();
+        };
+        cell.addEventListener("click", handleS);
     });
-    image2.addEventListener('click', () => {
-      cellaHTML[cellaID] = '<img src="cn.png" style="width="100%" height="100%">';
-      cella.innerHTML = cellaHTML[cellaID];
-      dialogS.close();
+}
+function aggiungiEventiDialoghi() {
+    image0.addEventListener("click", () => {
+        cPezzo("", cella);
+        dialogS.close();
     });
-    image3.addEventListener('click', () => {
-      cellaHTML[cellaID] = '<img src="tb.png" style="width="100%" height="100%">';
-      cella.innerHTML = cellaHTML[cellaID];
-      dialogS.close();
+    image1.addEventListener("click", () => {
+        cPezzo('<img src="cb.png" style="width="100%" height="100%">');
+        dialogS.close();
     });
-    image4.addEventListener('click', () => {
-      cellaHTML[cellaID] = '<img src="tn.png" style="width="100%" height="100%">';
-      cella.innerHTML = cellaHTML[cellaID];
-      dialogS.close();
+    image2.addEventListener("click", () => {
+        cPezzo('<img src="cn.png" style="width="100%" height="100%">');
+        dialogS.close();
     });
-    image5.addEventListener('click', () => {
-      cellaHTML[cellaID] = '<img src="rb.png" style="width="100%" height="100%">';
-      cella.innerHTML = cellaHTML[cellaID];
-      dialogS.close();
+    image3.addEventListener("click", () => {
+        cPezzo('<img src="tb.png" style="width="100%" height="100%">');
+        dialogS.close();
     });
-    image6.addEventListener('click', () => {
-      cellaHTML[cellaID] = '<img src="rn.png" style="width="100%" height="100%">';
-      cella.innerHTML = cellaHTML[cellaID];
-      dialogS.close();
+    image4.addEventListener("click", () => {
+        cPezzo('<img src="tn.png" style="width="100%" height="100%">');
+        dialogS.close();
     });
-    image7.addEventListener('click', () => {
-      cellaHTML[cellaID] = '<img src="qb.png" style="width="100%" height="100%">';
-      cella.innerHTML = cellaHTML[cellaID];
-      dialogS.close();
+    image5.addEventListener("click", () => {
+        cPezzo('<img src="rb.png" style="width="100%" height="100%">');
+        dialogS.close();
     });
-    image8.addEventListener('click', () => {
-      cellaHTML[cellaID] = '<img src="qn.png" style="width="100%" height="100%">';
-      cella.innerHTML = cellaHTML[cellaID];
-      dialogS.close();
+    image6.addEventListener("click", () => {
+        cPezzo('<img src="rn.png" style="width="100%" height="100%">');
+        dialogS.close();
     });
-    image9.addEventListener('click', () => {
-      dialogS.close();
+    image7.addEventListener("click", () => {
+        cPezzo('<img src="qb.png" style="width="100%" height="100%">');
+        dialogS.close();
     });
-celle_b = document.querySelectorAll(".cbordo");
-dialogB = document.getElementById('bordoDialog');
-celle_b.forEach(cell => {
-	cell.addEventListener('click', () => {
-	cellId = cell;
-	dialogB.showModal();
-	});
-});
-    image10.addEventListener('click', () => {
-      cellId.innerHTML = '<img src="p1b.png" style="width="100%" height="100%">';
-      dialogB.close();
+    image8.addEventListener("click", () => {
+        cPezzo('<img src="qn.png" style="width="100%" height="100%">');
+        dialogS.close();
     });
-    image11.addEventListener('click', () => {
-      cellId.innerHTML = '<img src="p1n.png" style="width="100%" height="100%">';
-      dialogB.close();
+    image9.addEventListener("click", () => {
+        dialogS.close();
     });
-    image12.addEventListener('click', () => {
-      cellId.innerHTML = '<img src="m1b.png" style="width="100%" height="100%">';
-      dialogB.close();
+    image10.addEventListener("click", () => {
+        cella.innerHTML =
+            '<img src="p1b.png" style="width="100%" height="100%">';
+        dialogB.close();
     });
-    image13.addEventListener('click', () => {
-      cellId.innerHTML = '<img src="m1n.png" style="width="100%" height="100%">';
-      dialogB.close();
+    image11.addEventListener("click", () => {
+        cella.innerHTML =
+            '<img src="p1n.png" style="width="100%" height="100%">';
+        dialogB.close();
     });
-    image14.addEventListener('click', () => {
-      dialogB.close();
+    image12.addEventListener("click", () => {
+        cella.innerHTML =
+            '<img src="m1b.png" style="width="100%" height="100%">';
+        dialogB.close();
+    });
+    image13.addEventListener("click", () => {
+        cella.innerHTML =
+            '<img src="m1n.png" style="width="100%" height="100%">';
+        dialogB.close();
+    });
+    image14.addEventListener("click", () => {
+        dialogB.close();
     });
 }
 // bottone "Impostazioni"
-document.getElementById("impostazioni").addEventListener(
-  "click", 
-  function() {
-    n = 6 + (n+1)%3;
+document.getElementById("impostazioni").addEventListener("click", function () {
+    rimuoviEventiScacchiera();
+    n = 6 + ((n + 1) % 3);
     creaScacchiera(n);
-    aggiungiEventi();
-    logga();
-  }
-);
+    aggiungiEventiScacchiera();
+    stampaUA();
+});
 // bottone "Mostra/Nascondi"
-document.getElementById("mostra").addEventListener(
-  "click", 
-  function() {
+document.getElementById("mostra").addEventListener("click", function () {
     if (mostra) {
         celle = document.getElementsByClassName("cinterno");
-	for (i = 0; i < celle.length; i++) {
-		celle[i].innerHTML = ""
+        for (i = 0; i < celle.length; i++) {
+            if (!cellaVSBL[celle[i].id]) {
+                celle[i].innerHTML = "";
+            }
         }
-    document.getElementById("mostra").textContent = 'Mostra';
+        document.getElementById("mostra").textContent = "Mostra";
     }
     if (!mostra) {
         celle = document.getElementsByClassName("cinterno");
-	for (i = 0; i < celle.length; i++) {
-		celle[i].innerHTML = cellaHTML[celle[i].id]
+        for (i = 0; i < celle.length; i++) {
+            celle[i].innerHTML = cellaHTML[celle[i].id];
         }
-    document.getElementById("mostra").textContent = 'Nascondi';
+        document.getElementById("mostra").textContent = "Nascondi";
     }
-	mostra = !mostra;
-     }
-);
+    mostra = !mostra;
+});
 //
-function logga() {
-	statW = "Scacchiera; " + scacchieraW + ", " + n + ", " + lcella + "<br />";
-    	storicoDiv.insertAdjacentHTML("afterbegin",statW);
-}
 function stampaUA() {
-	storicoDiv.insertAdjacentHTML("afterbegin","<br />" + window.navigator.userAgent + "<br />");
-	statS = "Schermo: "+ wWindow + ", " + hWindow + "<br />";
-    	storicoDiv.insertAdjacentHTML("afterbegin",statS);
+    storicoDiv.innerHTML = "";
+    storicoDiv.insertAdjacentHTML("beforeend", "<br />--- fine ---");
+    storicoDiv.insertAdjacentHTML(
+        "afterbegin",
+        "<br />" + window.navigator.userAgent + "<br />"
+    );
+    statS = "Schermo: " + wWindow + ", " + hWindow + "<br />";
+    storicoDiv.insertAdjacentHTML("afterbegin", statS);
+    statW = "Scacchiera; " + scacchieraW + ", " + n + ", " + lcella + "<br />";
+    storicoDiv.insertAdjacentHTML("afterbegin", statW);
+}
+// inserisci o verifica il pezzo nella cella
+function cPezzo(cHTML) {
+    if (mostra) {
+        cellaHTML[cellaID] = cHTML;
+        cella.innerHTML = cellaHTML[cellaID];
+    }
+    if (!mostra) {
+        if (cHTML == cellaHTML[cellaID]) {
+            cellaVSBL[cellaID] = true;
+            cella.innerHTML = cellaHTML[cellaID];
+            storicoDiv.insertAdjacentHTML(
+                "afterbegin",
+                "indovinato <br />"
+            );
+        } else {
+             storicoDiv.insertAdjacentHTML(
+                "afterbegin",
+                "sbagliato <br />"
+            );
+        }
+    }
 }
