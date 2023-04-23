b7335708603470736a87abdcf32706f0331e7fe6
clevermindgame <130675847+clevermindgame@users.noreply.github.com>
2023-04-23 14:02:58 +0200

    Updated from Publii

--- media/files/gscript.js
+++ media/files/gscript.js
@@ -2,7 +2,7 @@
 hWindow = window.innerHeight;
 wWindow = window.innerWidth;
 rWindow = hWindow / wWindow;
-if (hWindow < wWindow) {alert('Per una migliore fruibilità del gioco ti sugegriamo di ruotare il dispositivo')}
+if (hWindow < wWindow) {alert('Per una migliore fruibilità del gioco ti suggeriamo di ruotare il dispositivo e ricaricare la pagina.')}
 //
 palette = [['#FAF6E0','#000000'],['#E0FF85','#000000'],['#89FFB6','#760049'],['#89FFB6','#000000'],['#F5ECD0','#000000'],['#F2E2BA','#000000']];
 paletteID = 0;
