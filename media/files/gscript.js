4e58580a3bdd12f437c399a44ecb32a4b7bf098c
clevermindgame <130675847+clevermindgame@users.noreply.github.com>
2023-04-25 08:38:03 +0200

    Updated from Publii

--- media/files/gscript.js
+++ media/files/gscript.js
@@ -6,11 +6,9 @@ rWindow = hWindow / wWindow;
 if (wWindow > hWindow) {
     const dialog = document.querySelector('#ruotaDispositivo');
     const closeButton = document.querySelector('#okB');
-
     document.addEventListener('DOMContentLoaded', () => {
       dialog.showModal();
     });
-
     closeButton.addEventListener('click', () => {
       dialog.close();
     });
@@ -22,6 +20,9 @@ paletteID = 0;
 cellaHTML = new Array(64);
 cellaVSBL = new Array(64);
 cellaRegole = new Array(64);
+cellaRegoleTemp = new Array(64);
+idStart = 0;
+idEnd = 0;
 //
 // Adatta la scacchiera alla dimensione dello schermo
 // fino a un rapporto H/W di 1.4, la larghezza della scacchiera è dell'80%
@@ -194,21 +195,25 @@ function aggiungiEventiDialoghi() {
     image10.addEventListener("click", () => {
         cella.innerHTML =
             '<img src="p1b.png" style="width="100%" height="100%">';
+	    percorso(cellaID,1,0);
         dialogB.close();
     });
     image11.addEventListener("click", () => {
         cella.innerHTML =
             '<img src="p1n.png" style="width="100%" height="100%">';
+	    percorso(cellaID,1,1);
         dialogB.close();
     });
     image12.addEventListener("click", () => {
         cella.innerHTML =
             '<img src="m1b.png" style="width="100%" height="100%">';
-        dialogB.close();
+ 	    percorso(cellaID,-1,0);
+       dialogB.close();
     });
     image13.addEventListener("click", () => {
         cella.innerHTML =
             '<img src="m1n.png" style="width="100%" height="100%">';
+	    percorso(cellaID,-1,1);
         dialogB.close();
     });
     image14.addEventListener("click", () => {
@@ -261,12 +266,14 @@ function stampaUA() {
     storicoDiv.insertAdjacentHTML("afterbegin", statS);
     statW = "Scacchiera; " + scacchieraW + ", " + n + ", " + lcella + "<br />";
     storicoDiv.insertAdjacentHTML("afterbegin", statW);
+    storicoDiv.insertAdjacentHTML("afterbegin", "<br />");
 }
 // inserisci o verifica il pezzo nella cella
 function cPezzo(cHTML,p) {
     if (mostra) {
         cellaHTML[cellaID] = cHTML;
         cella.innerHTML = cellaHTML[cellaID];
+        ripristina();
 // regole del pezzo
 // [dirR,dirC,moltiplica_colore,aggiungi,moltiplica]
         switch (p) {
@@ -314,3 +321,69 @@ function cPezzo(cHTML,p) {
         }
     }
 }
+function deepCopyArray(arr) {
+  const copy = [];
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    if (Array.isArray(element)) {
+      copy[i] = deepCopyArray(element); // recursively copy nested array
+    } else if (typeof element === 'object' && element !== null) {
+      copy[i] = deepCopyObject(element); // recursively copy nested object
+    } else {
+      copy[i] = element; // copy primitive value
+    }
+  }
+  return copy;
+}
+function percorso(cinID,valore,colore) {
+    ripristina();
+    idStart = cinID;
+// stato = [dir_r,dir_c,valore,colore]
+// regola = [new_dir_r,new_dir_c,moltiplica_colore,somma,moltiplica]
+    npassi = 0;
+    nrimbalzi = 0;
+    cellaRegoleTemp = deepCopyArray(cellaRegole);
+    j = cinID % n;
+    i = Math.round((cinID-j)/n);
+    if (valore === 1) {valstring ="+1"} else {valstring = valore};
+    if (colore === 0) {colstring = "b"} else {colstring = "n"};
+    cin = valstring + colstring + " in " + String.fromCharCode(j + 65, i + 49) + " >> ";
+    if (colore === 0) {colore = -1};
+    if (i == 0) {stato = [1,0,valore,colore]};
+    if (i == n-1) {stato = [-1,0,valore,colore]};
+    if (j == 0) {stato = [0,1,valore,colore]};
+    if (j == n-1) {stato = [0,-1,valore,colore]};
+    regola = cellaRegoleTemp[cinID];
+//
+   do{
+       rimb = false;
+       if (regola[0] === '') {ii = stato[0]} else {rimb=true; ii = regola[0]};
+       if (regola[1] === '') {jj = stato[1]} else {rimb=true; jj = regola[1]};
+       if (rimb) {nrimbalzi += 1};
+       cellaRegoleTemp[n*i+j] = ['','',1,0,1];
+       i += ii;
+       j += jj;
+       colore *= regola[2];
+       valore = (valore + regola[3])*regola[4];
+       stato = [ii,jj,valore,colore];
+       regola = cellaRegoleTemp[n*i + j];
+       npassi += 1;
+    } while ((i != 0) & (i != 7) & (j != 0) & (j!= 7));
+    idEnd = n*i+j;
+    if (colore === -1) {
+       uscita = '<img src="uscita_bianca.png" style="width="100%" height="100%">';
+    } else {
+       uscita = '<img src="uscita_nera.png" style="width="100%" height="100%">';
+}
+    document.getElementById(idEnd).innerHTML = uscita;
+    if (colore === -1) {colstring = "b"} else {colstring = "n"};
+    if (valore > 0) {valore = "+"+valore};
+    if (nrimbalzi === 1){rimbstring = " rimbalzo"} else {rimbstring = " rimbalzi"};
+    cout = valore+colstring+" in "+String.fromCharCode(j+65,i+49)+", "+npassi+" passi e "+nrimbalzi+rimbstring+"<br />"
+    storicoDiv.insertAdjacentHTML("afterbegin", cin + cout);
+}
+function ripristina() {
+	document.getElementById(idStart).innerHTML = cellaHTML[idStart];
+	document.getElementById(idEnd).innerHTML = cellaHTML[idEnd];
+console.log(idStart,idEnd);
+	}
